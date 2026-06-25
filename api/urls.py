b06b2db786d27f306036454158beba524b30fb28
DivyeshGupta08 <divyeshgupta031@gmail.com>
2026-06-25 23:09:50 +0530

    Step 18 Authentication API Completed

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import topic_api, quiz_api, question_api, topic_quizzes_api, quiz_questions_api
+from .views import topic_api, quiz_api, question_api, topic_quizzes_api, quiz_questions_api, login_api, profile_api
 
 
 urlpatterns = [
@@ -33,6 +33,18 @@ urlpatterns = [
         'quizzes/<int:quiz_id>/questions/',
         quiz_questions_api,
         name='quiz_questions_api'
+    ),
+    
+    path(
+        'login/',
+        login_api,
+        name='login_api'
+    ),
+    
+    path(
+      'profile/',
+        profile_api,
+        name='profile_api'
     ),
 
 ]
--- api/views.py
+++ api/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, get_object_or_404
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
-
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
 from quizzes.models import Topic, Quiz, Question
-
 from .serializers import TopicSerializer, QuizSerializer, QuestionSerializer
-
 
 @api_view(['GET'])
 def topic_api(request):
@@ -96,3 +97,54 @@ def quiz_questions_api(
     return Response(
         serializer.data
     )
+    
+@api_view(['POST'])
+def login_api(request):
+
+    username = request.data.get(
+        'username'
+    )
+
+    password = request.data.get(
+        'password'
+    )
+
+    user = authenticate(
+        username=username,
+        password=password
+    )
+
+    if user:
+
+        token, created = Token.objects.get_or_create(
+            user=user
+        )
+
+        return Response({
+
+            'token': token.key,
+
+            'username': user.username
+
+        })
+
+    return Response({
+
+        'error': 'Invalid Credentials'
+
+    })
+    
+@api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def profile_api(request):
+
+    return Response({
+
+        'username': request.user.username,
+
+        'email': request.user.email,
+
+        'id': request.user.id
+
+    })
