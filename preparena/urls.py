a0b2675e330ef80241a9ed7191d4d86504396790
DivyeshGupta08 <divyeshgupta031@gmail.com>
2026-06-26 22:41:10 +0530

    Step 26 - Swagger API Documentation completed

--- preparena/urls.py
+++ preparena/urls.py
@@ -3,16 +3,71 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework import permissions
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+
+    openapi.Info(
+
+        title="PrepArena API",
+
+        default_version='v1',
+
+        description="API Documentation for PrepArena",
+
+    ),
+
+    public=True,
+
+    permission_classes=[permissions.AllowAny],
+
+)
+
+
 urlpatterns = [
+
     path('admin/', admin.site.urls),
 
     path('', include('accounts.urls')),
+
     path('quizzes/', include('quizzes.urls')),
+
     path('dashboard/', include('dashboard.urls')),
+
     path('leaderboard/', include('leaderboard.urls')),
+
     path('api/', include('api.urls')),
+
+    path(
+        'swagger/',
+        schema_view.with_ui(
+            'swagger',
+            cache_timeout=0
+        ),
+        name='swagger-ui'
+    ),
+
+    path(
+        'redoc/',
+        schema_view.with_ui(
+            'redoc',
+            cache_timeout=0
+        ),
+        name='redoc'
+    ),
+
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+
+    urlpatterns += static(
+
+        settings.MEDIA_URL,
+
+        document_root=settings.MEDIA_ROOT
+
+    )
